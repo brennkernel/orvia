bc5157c41081e548b3b985c1a9bd18d2c8f4fc26
Oscar Jane <hello@oscar-jane.com>
2025-04-18 11:27:35 +0200

    style: update design tokens and Tailwind theme with new gray palette

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -10,13 +10,11 @@ export default {
       fontFamily: {
         sans: ['var(--font-inter)', ...fontFamily.sans],
       },
-
       borderRadius: {
         lg: 'var(--radius)',
         md: 'calc(var(--radius) - 2px)',
         sm: 'calc(var(--radius) - 4px)',
       },
-
       colors: {
         background: 'hsl(var(--background))',
         foreground: 'hsl(var(--foreground))',
@@ -51,6 +49,29 @@ export default {
         border: 'hsl(var(--border))',
         input: 'hsl(var(--input))',
         ring: 'hsl(var(--ring))',
+      },
+
+      keyframes: {
+        'accordion-down': {
+          from: {
+            height: '0',
+          },
+          to: {
+            height: 'var(--radix-accordion-content-height)',
+          },
+        },
+        'accordion-up': {
+          from: {
+            height: 'var(--radix-accordion-content-height)',
+          },
+          to: {
+            height: '0',
+          },
+        },
+      },
+      animation: {
+        'accordion-down': 'accordion-down 0.2s ease-out',
+        'accordion-up': 'accordion-up 0.2s ease-out',
       },
     },
   },
