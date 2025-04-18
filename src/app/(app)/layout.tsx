999d766d40b662683f0e1f652835705004753fdb
Oscar Jane <hello@oscar-jane.com>
2025-04-18 11:35:23 +0200

    chore: move app into (app) group folder to separate from landing

---
+++ src/app/(app)/layout.tsx
@@ -0,0 +1,31 @@
+import '@/styles/globals.css'
+
+import { Inter } from 'next/font/google'
+import { type Metadata } from 'next'
+
+import { TRPCReactProvider } from '@/trpc/react'
+
+export const metadata: Metadata = {
+  title: 'Orvia — Smarter Forms for Notion',
+  description:
+    'Orvia lets you show or hide fields, validate responses, and ensure cleaner, more complete form submissions — without breaking your Notion flow.',
+  // icons: [{ rel: 'icon', url: '/favicon.ico' }],
+}
+
+const inter = Inter({
+  subsets: ['latin'],
+  display: 'swap',
+  variable: '--font-inter',
+})
+
+export default function RootLayout({
+  children,
+}: Readonly<{ children: React.ReactNode }>) {
+  return (
+    <html lang="en" className={inter.variable}>
+      <body>
+        <TRPCReactProvider>{children}</TRPCReactProvider>
+      </body>
+    </html>
+  )
+}
